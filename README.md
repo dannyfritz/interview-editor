367a14dde2494e3238e6a20f5764aaf1bfdeb584
Danny Fritz <dannyfritz@gmail.com>
2022-09-29 16:26:44 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,48 @@
+# Editor Interview Exercise
+
+A TDD based interview.
+
+Origin of this interview is unknown, but it started when I worked at Galvanize Inc in about 2015.
+Possibly originated from Jeff Dean.
+
+## Parties Involved
+
+- _Interviewer_: The person conducting the interview. The driver.
+- _Interviewee_: The person being interviewwd. The navigator.
+
+## Process
+
+### Pre-Interview Prep
+
+1. Clone or reset interview repo
+1. Run the test to ensure it is ready to go. It should fail with 1 test run any many skipped.
+1. Open the repo in the editor of the Interviewer's choice
+
+### Interview
+
+1. Introductions
+   - Name
+   - Brief employment history
+   - Favorite bird
+1. Explanation of process to interviewee
+1. Interviewer shares screen
+1. Pairing begins
+   - Using search engines is allowed, but should be done on the interviewer's shared scren
+   - Interviewee is the navigator of the programming session
+   - Interviewer is the driver of the programming session
+   - Each time a test passes, turn on the next test
+   - Tests are intentionally increasing in difficulty
+   - The interviewer is able to aid when interviewee is stuck
+1. Time ends
+1. Debrief interviewee
+
+### Assessment
+
+| Area            | Bad                                             | Good                                   | Great                                     |
+| --------------- | ----------------------------------------------- | -------------------------------------- | ----------------------------------------- |
+| JS/TS Knowledge | Needed to rely on interviewer for all knowledge | Knew methods but looked up specifics   | Didn't need to rely on the internet much  |
+| Problem Solving | Could not get passed the first 3 tests          | Passed all tests except undo/redo      | Passed all tests                          |
+| Collaboration   | Was not receptive to ideas from the interviewer | Receptive, but did not ask for help    | Knew when to ask for help                 |
+| Friendliness    | Was not nice                                    | Was nice                               | You liked this person                     |
+| Communication   | Never spoke except to give commands             | Sometimes knew what they were thinking | Was never a guess what they were thinking |
+| Patience        | Constant interuption                            | Some interuption                       | Few or no interuptions                    |
---
+++ test/editor.test.ts
@@ -0,0 +1,112 @@
+import { describe, expect, test } from "vitest";
+
+import { Editor } from "../src/editor";
+
+describe("Editor", () => {
+  test("User can create an Editor", () => {
+    const editor = new Editor();
+
+		expect(editor).toBeInstanceOf(Editor);
+  });
+
+	test.skip("User can write text to Editor", () => {
+		const editor = new Editor();
+
+		editor.write('Hello - codez');
+    expect(editor.toString()).to.equal('Hello - codez');
+
+    editor.write(' moar');
+    expect(editor.toString()).to.equal('Hello - codez moar');
+	})
+
+	test.skip('Users can undo writes', () => {
+    const editor = new Editor();
+
+    editor.write('Hello - codez');
+    expect(editor.toString()).to.equal('Hello - codez');
+
+    editor.write('Moar stuff');
+    expect(editor.toString()).to.equal('Hello - codezMoar stuff');
+
+    editor.write('Even more');
+    expect(editor.toString()).to.equal('Hello - codezMoar stuffEven more');
+
+    editor.undo();
+    expect(editor.toString()).to.equal('Hello - codezMoar stuff');
+
+    editor.undo();
+    expect(editor.toString()).to.equal('Hello - codez');
+
+    editor.undo();
+    expect(editor.toString()).to.equal('');
+  });
+
+	test.skip('Users can find and replace text', () => {
+    const editor = new Editor();
+
+    editor.write('foo stuff');
+    editor.write(' other foo');
+    editor.replace('foo', 'bar');
+    expect(editor.toString()).to.equal('bar stuff other bar');
+  });
+
+  test.skip('Users can undo find and replace', () => {
+    const editor = new Editor();
+
+    editor.write('foo stuff');
+    editor.write(' other foo');
+    editor.replace('foo', 'bar');
+    expect(editor.toString()).to.equal('bar stuff other bar');
+
+    editor.undo();
+    expect(editor.toString()).to.equal('foo stuff other foo');
+
+    editor.undo();
+    expect(editor.toString()).to.equal('foo stuff');
+  });
+
+  test.skip('Users can redo actions', () => {
+    const editor = new Editor();
+
+    editor.write('foo stuff');
+    editor.write(' other foo');
+    editor.replace('foo', 'bar');
+    expect(editor.toString()).to.equal('bar stuff other bar');
+
+    editor.undo();
+    expect(editor.toString()).to.equal('foo stuff other foo');
+
+    editor.undo();
+    expect(editor.toString()).to.equal('foo stuff');
+
+    editor.redo();
+    expect(editor.toString()).to.equal('foo stuff other foo');
+
+    editor.redo();
+    expect(editor.toString()).to.equal('bar stuff other bar');
+  });
+
+  test.skip('Users cannot redo after performing an action', () => {
+    const editor = new Editor();
+
+    editor.write('foo stuff');
+    editor.write(' other foo');
+    editor.replace('foo', 'bar');
+    expect(editor.toString()).to.equal('bar stuff other bar');
+
+    editor.undo();
+    expect(editor.toString()).to.equal('foo stuff other foo');
+
+    editor.undo();
+    expect(editor.toString()).to.equal('foo stuff');
+
+    editor.redo();
+    expect(editor.toString()).to.equal('foo stuff other foo');
+
+		editor.write(" Hello");
+    expect(editor.toString()).to.equal('foo stuff other foo Hello');
+
+    editor.redo();
+    expect(editor.toString()).to.equal('foo stuff other foo Hello');
+  });
+});
